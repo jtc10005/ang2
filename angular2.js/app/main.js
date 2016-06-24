6b1cdf90d2c141264216b7511bb38b7d67ccd4e4
Jay Culpepper <jtculpepper@buckman.com>
2016-06-24 12:26:26 -0500

    removed es6 and added js project

---
+++ angular2.js/app/main.js
@@ -0,0 +1,5 @@
+﻿(function (app) {
+  document.addEventListener('DOMContentLoaded', function () {
+    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+  });
+})(window.app || (window.app = {}));
